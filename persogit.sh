fd2262686d7e12c951eb0f7a5524f57f38b2a0d5
taslim <harishammoud@gmail.com>
2022-01-02 14:53:28 +0100

    Create persogit.sh

---
+++ persogit.sh
@@ -0,0 +1,31 @@
+## Scripte d'import des outils de hacks que j'utilise régulièrement ##
+## !! NE PAS UTILISER SANS LE CONSANTEMENT DES VITCIMES !!  ##
+
+# Install de Hack9Tools
+
+git clone https://github.com/xHak9x/Hak9Tools.git
+cd Hak9Tools
+chmod +x hak9tools.sh
+
+## install de phoneifoga 
+
+docker pull sundowndev/phoneinfoga:latest
+# to run docker run --rm -it sundowndev/phoneinfoga version
+
+# installation de sherlock 
+
+git clone https://github.com/sherlock-project/sherlock.git
+
+# change the working directory to sherlock
+cd sherlock
+
+# install the requirements
+python3 -m pip install -r requirements.txt
+
+
+#install de social phish 
+
+git clone https://github.com/xHak9x/SocialPhish.git
+cd SocialPhish
+chmod +x socialphish.sh
+
